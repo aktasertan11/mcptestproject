120556f179cc2d19fe244971d8ce5149c690a1b4
aktasertan11 <aktasertan11@gmail.com>
2025-06-19 00:55:21 +0300

    commit 4

--- reviewMCP/Program.cs
+++ reviewMCP/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using ModelContextProtocol.Server;
 using System.ComponentModel;
 using System.Collections.Generic;
+using System.IO;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Logging.AddConsole(consoleLogOptions =>
@@ -27,9 +28,12 @@ public static class EchoTool
     public static string Reverse(string message) => $"Reversed: {new string(message.Reverse().ToArray())}";
 
     [McpServerTool, Description("Reviews the given commit diff and returns a code review comment.")]
-    public static string ReviewCommit(string diff)
+    public static string ReviewCommit(string diffFilePath)
     {
-        // Basit örnek: TODO, fixme, var gibi anahtar kelimeleri uyarı olarak döndür
+        // diffFilePath ile dosya içeriğini oku
+        if (!File.Exists(diffFilePath))
+            return $"Diff file not found: {diffFilePath}";
+        var diff = File.ReadAllText(diffFilePath);
         var comments = new List<string>();
         if (diff.Contains("TODO", StringComparison.OrdinalIgnoreCase))
             comments.Add("Found TODO in code. Please address before merging.");
